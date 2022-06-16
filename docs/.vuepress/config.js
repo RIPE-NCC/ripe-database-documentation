f9599cbfa11453d579fed17cf5280f725b9bc91b
ssuess <github@stephensuess.com>
2022-06-16 11:32:39 +0200

    add favicons to config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -33,6 +33,12 @@ module.exports = {
   description: "The RIPEstat Docs Center",
   dest: process.env.BETA ? 'builds/docsbeta' : 'builds/docs',
   base: process.env.BETA ? '/docsbeta/' : '/docs/',
+  head: [
+    ['link', { rel: "icon", type: "image/png", sizes: "128x128", href: "/icons/favicon-128x128.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "96x96", href: "/icons/favicon-96x96.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/icons/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/icons/favicon-16x16.png"}],
+  ],
   themeConfig: {
     nav:[],
     sidebar,
