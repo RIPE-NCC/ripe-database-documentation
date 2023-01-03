2541fb07f29b8587261b6ac94d48eacc3e443ce4
mherran <mherran@ripe.net>
2023-01-03 11:35:49 +0100

    feat: add default zoom

--- docs/.vuepress/theme/components/SvgZoom.vue
+++ docs/.vuepress/theme/components/SvgZoom.vue
@@ -7,13 +7,13 @@ export default {
         window.addEventListener('load', function () {
             var svgs = d3.selectAll("svg[id^='mermaid']");
             svgs.each(function() {
-            var svg = d3.select(this);
-            svg.html("<g>" + svg.html() + "</g>");
-            var inner = svg.select("g");
-            var zoom = d3.zoom().on("zoom", function(event) {
-                inner.attr("transform", event.transform);
-            });
-            svg.call(zoom);
+                var svg = d3.select(this);
+                svg.html("<g>" + svg.html() + "</g>");
+                var inner = svg.select("g");
+                var zoom = d3.zoom().on("zoom", function(event) {
+                    inner.attr("transform", event.transform);
+                });
+                svg.call(zoom).call(zoom.transform, d3.zoomIdentity.translate(-880.3226965549293,-755.7167138457196).scale(1.7195129724860199));
             });
         });
     }
