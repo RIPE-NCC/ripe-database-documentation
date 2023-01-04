e5814cee1466d33734fdf20d736bf6d120529ab7
mherran <mherran@ripe.net>
2023-01-04 09:37:39 +0100

    feat: attach the buttons, does leave to attach the buttons in same svg div

--- docs/.vuepress/theme/components/SvgZoom.vue
+++ docs/.vuepress/theme/components/SvgZoom.vue
@@ -5,17 +5,56 @@ export default {
     name: "svgZoom",
     mounted() {
         window.addEventListener('load', function () {
-            var svgs = d3.selectAll("svg[id^='mermaid']");
-            svgs.each(function() {
-                var svg = d3.select(this);
-                svg.html("<g>" + svg.html() + "</g>");
-                var inner = svg.select("g");
-                var zoom = d3.zoom().on("zoom", function(event) {
-                    inner.attr("transform", event.transform);
-                });
-                svg.call(zoom).call(zoom.transform, d3.zoomIdentity.translate(-880.3226965549293,-755.7167138457196).scale(1.7195129724860199));
+            // Create buttons
+            let div = document.querySelector("div[id^='mermaid']");
+            let p = document.createElement("p");
+
+            let zoom_in = document.createElement("button");
+            zoom_in.type="submit"
+            zoom_in.name="zoom_in";
+            zoom_in.innerHTML="Zoom in";
+            zoom_in.id="zoom_in";
+
+            console.log(div)
+
+            let zoom_out = document.createElement("button");
+            zoom_out.type="submit"
+            zoom_out.name="zoom_out";
+            zoom_out.innerHTML="Zoom out";
+            zoom_out.id="zoom_out";
+
+            p.append(zoom_in); 
+            p.append(zoom_out); 
+            div.parentNode.insertBefore(p,div);
+
+
+            //Zoom logic
+
+            console.log("a1")
+            //let zoom = d3.zoom().on("zoom", zoomed);
+
+            let svg = d3.select("svg[id^='mermaid']")
+                .attr("width", "100%")
+                .attr("height", "100%")
+                .style("background-color", "#eeeeee");
+            svg.html("<g>" + svg.html() + "</g>");
+            var inner = svg.select("g");
+
+            var zoom = d3.zoom().on("zoom", function(event) {
+                inner.attr("transform", event.transform);
             });
+            /*d3.select("#zoom_in").on("click", function() {
+                console.log("zoom in");
+                zoom.scaleBy(svg.transition().duration(750), 1.2);
+            });
+            d3.select("#zoom_out").on("click", function() {
+                console.log("zoom out");
+                zoom.scaleBy(svg.transition().duration(750), 0.8);
+            });*/
+
+            svg.call(zoom).call(zoom.transform, d3.zoomIdentity.translate(-880.3226965549293,-755.7167138457196).scale(1.7195129724860199));
         });
+        
     }
 }
 </script>
