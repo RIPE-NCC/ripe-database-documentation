e770ebe58ce4cab3bda1e8cfd32c5c3988b7bc02
mherran <mherran@ripe.net>
2025-06-18 15:03:44 +0200

    feat: apply search bar component

---
+++ .vitepress/components/SearchBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="search-bar">
+    <SearchPlugin />
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import { SearchPlugin } from "@swe-database/ncc-vitepress-plugin-search";
+</script>
--- .vitepress/theme/layouts/Page.vue
+++ .vitepress/theme/layouts/Page.vue
@@ -3,6 +3,7 @@
 import { defineProps } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import { useData } from 'vitepress'
+import SearchBar from "../../components/SearchBar.vue";
 
 const { frontmatter } = useData()
 
@@ -18,6 +19,7 @@ defineProps({
   <DefaultTheme.Layout>
     <!-- Your custom doc content -->
     <template #doc-before>
+      <search-bar/>
       <div style="background-color: yellow; padding: 20px; border: 5px solid red; margin-bottom: 20px;">
         <h1>{{ frontmatter.title || "Custom Doc" }}</h1>
       </div>
